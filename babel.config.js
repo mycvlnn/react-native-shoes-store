04927488d6dfa7228f05971eb9e6e26532c613e0
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-07-02 15:06:12 +0700

    feat: xóa những file không cần thay đổi ở git và cấu hình thêm alias file

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,15 @@ module.exports = {
       {
         alias: {
           '~': './src',
+          '~components': './src/components',
+          '~layouts': './src/layouts',
+          '~configs': './src/configs',
+          '~store': './src/store',
+          '~assets': './src/assets',
+          '~views': './src/views',
+          '~routes': './src/routes',
         },
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
       },
     ],
   ],
