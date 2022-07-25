db665474ecfe601882371a9b5fc2e2d5c86a8527
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-07-25 21:03:31 +0700

    fix: không thể tìm thấy modudle khi import

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module '*.jpg' {
+  import { ImageSourcePropType } from 'react-native'
+
+  const content: ImageSourcePropType
+
+  export default content
+}
+
+declare module '*.png' {
+  import { ImageSourcePropType } from 'react-native'
+
+  const content: ImageSourcePropType
+
+  export default content
+}
+
+declare module '*.svg' {
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
+
+  const content: React.FC<SvgProps>
+
+  export default content
+}
